a308da0ede865d04bcb49efcf856dd3675fd3ce4
ryanoliveir <ryandehallo229@gmail.com>
2022-12-15 22:09:12 -0300

    some fix

--- main.cpp
+++ main.cpp
@@ -7,15 +7,17 @@ using namespace std;
 void header();
 void clear();
 void enterInput();
-float floatInput();
-void minimunCostCalc(float* widthRiverBank,float* distance, float* earthCost, float* waterCost, float* cost);
+double doubleInput();
+void minimunCostCalc(double* widthRiverBank,double* distance, double* earthCost, double* waterCost, double* cost, double* riverDistance, double* landDistance);
 
 int main(){
 
-    float widthRiverBank,
+    double widthRiverBank,
     distance,
     earthCost,
     waterCost,
+    landDistance,
+    riverDistance,
     cost = 0;
 
     do{
@@ -25,18 +27,26 @@ int main(){
 
         cout << "[*] Please provide the following information:" << endl;
         cout << "[*] Width of river bank (m): ";
-        widthRiverBank = floatInput();
+        widthRiverBank = doubleInput();
         cout << "[*] Distance from the Plant (m): ";
-        distance = floatInput();
+        distance = doubleInput();
         cout << "[*] Cable cost in..." << endl;
         cout << "[*] Earth ($): ";
-        earthCost = floatInput();
+        earthCost = doubleInput();
         cout << "[*] Water ($): ";
-        waterCost = floatInput();
+        waterCost = doubleInput();
         // cout << "[*] Type 'q' to exit." << endl;
 
-        minimunCostCalc(&widthRiverBank,&distance, &earthCost, &waterCost,&cost);
-        
+        minimunCostCalc(&widthRiverBank, &distance, &earthCost, &waterCost,&cost, &riverDistance, &landDistance);
+
+
+        cout << "[---------------------------------------------------]" << endl;
+        printf("[+] MINIMUN COST TO OPERATION: R$ %.2f\n", abs(cost) );
+        printf("[+] Distance in land: %.2f\n", abs(landDistance) );        
+        printf("[+] Distance in river: %.2f\n", abs(riverDistance) );
+        cout << ("[+] f(x) = 5.[(900^2 + x^2)^0,5] + 4.(300 - x) ") << endl;
+        cout << "[---------------------------------------------------]" << endl;
+        enterInput();
     } while (true);
    
 
@@ -58,21 +68,28 @@ void clear()
     header();
 }
 
-float floatInput(){
-    float data;
+double doubleInput(){
+    double data;
     cin >> data;
     return data;
         
 }
 
-void minimunCostCalc(float* widthRiverBank,float* distance, float* earthCost, float* waterCost, float* cost){
-    cout << *widthRiverBank << endl;
-    cout << *distance << endl;
-    cout << *earthCost << endl;
-    cout << *waterCost << endl;
-    cout << *cost << endl;
+void minimunCostCalc(double* widthRiverBank,double* distance, double* earthCost, double* waterCost, double* cost, double* riverDistance, double* landDistance){
 
-    enterInput();
+    double auxEarth,
+    auxWater,
+    auxiliary_variable,
+    x;
+
+    auxWater = pow(*waterCost,2); 
+    auxEarth = pow(*earthCost,2); 
+    auxiliary_variable = abs( auxWater - auxEarth); 
+    x = sqrt(auxEarth * pow(*widthRiverBank, 2) / auxiliary_variable);
+    *riverDistance = sqrt(pow(*widthRiverBank, 2) + pow(x,2));
+    *landDistance = *distance - x;
+
+    *cost = (*waterCost * *riverDistance) + (*earthCost * *landDistance);    
 
 }
 
